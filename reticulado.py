de10c566754c5af0409f49ad2f13165a50928502
JTWV98 <jtweihrauch@miuandes.cl>
2021-09-22 23:00:38 -0300

    Update reticulado.py

--- reticulado.py
+++ reticulado.py
@@ -129,40 +129,5 @@ class Reticulado(object):
 
     def __str__(self):
         
-        s="nodos: \n"
-        for i in range(self.Nnodos):
-            s+=f"\t {i}: ({self.xyz[i][0]} {self.xyz[i][1]} {self.xyz[i][2]}) \n"
-        s+="\n"
-        
-        s+="barras: \n"
-        for i,j in enumerate(self.barras,start=0):
-            s+=f"\t {i}: [{j.ni} {j.nj}] \n"
-        s+="\n"
-        
-        s+="restricciones: \n"
-        for i in self.restricciones:
-            s+=f"\t {i}: {self.restricciones[i]} \n"
-        s+="\n"
-        
-        s+="cargas: \n"
-        for i in self.cargas:
-            s+=f"\t {i}: {self.cargas[i]} \n"
-        s+="\n"
-        
-        s+="desplazamientos: \n"
-        i=0
-        j=0
-        while i < (len(self.u)):
-            s+=f"\t {j}: ({(self.u[i])}, {(self.u[i+1])}, {(self.u[i+2])}) \n"
-            i+=3
-            j+=1
-        s+="\n"
-        
-        s+="fuerzas: \n" 
-        for i,j in enumerate(self.barras,start=0):
-            s+=f"\t {i}: {j.obtener_fuerza(self)} \n"
-        s+="\n"
-        
-        
-        return s
+      
    
